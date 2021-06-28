52fb6db8a33680c5e983a7babefd6d52544fa92f
tamaracabello <tamaracabello7@gmail.com>
2021-06-27 21:38:30 -0300

    product images fix

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -5,7 +5,7 @@ exports.createNewProducto = async (req, res) => {
   const producto = req.body;
   try {
     await productosService.createProducto(producto);
-    return res.status(201).json({status:201, msg: "Nuevo producto cargado con éxito"});
+    return res.status(201).json({ status: 201, msg: "Nuevo producto cargado con éxito" });
   } catch (e) {
     return res.status(500).send("Error interno del servidor");
   }
@@ -14,16 +14,25 @@ exports.createNewProducto = async (req, res) => {
 
 /**
  * @description trae todas los productos por cliente
- * @param req.
- * @param res.
+ * @param req
+ * @param res
  */
- exports.getAllProductsByIdClient = async (req, res) => {
+exports.getAllProductsByIdClient = async (req, res) => {
   try {
-    const productos = await productosService.getAllProductsByIdClient(req.params.id);
-    if (productos.length > 0) return res.status(200).json({productos});
-    return res.status(204).send('No hay productos');
+    let productos = await productosService.getAllProductsByIdClient(req.params.id);
+    if (productos.length > 0) {
+      const newProducto = await Promise.all(productos.map(async p => {
+        try {
+          p.fotos = await subastasService.getImagesByProductoId(p.idProducto);
+          return p;
+        } catch (e) {
+          res.status(500).send('Error interno del servidor');
+        }
+      }))
+      res.status(200).send(newProducto);
+    } else return res.status(204).send('No hay productos');
   } catch (e) {
-    res.status(500).send('Error interno del servidor');
+    return res.status(500).send('Error interno del servidor');
   }
 }
 
@@ -32,10 +41,10 @@ exports.createNewProducto = async (req, res) => {
  * @param req.
  * @param res.
  */
- exports.getImagesByProductoId = async (req, res) => {
+exports.getImagesByProductoId = async (req, res) => {
   try {
     const fotos = await subastasService.getImagesByProductoId(req.params.id);
-    if (fotos.length > 0) return res.status(200).json({fotos});
+    if (fotos.length > 0) return res.status(200).json({ fotos });
     return res.status(204).send('No hay contenido');
   } catch (e) {
     res.status(500).send('Error interno del servidor');

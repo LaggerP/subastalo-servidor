4626245c2116f8a18e1b9ec3204230d68771b157
tamaracabello <tamaracabello7@gmail.com>
2021-07-01 17:35:56 -0300

    update

--- routes/subastas.js
+++ routes/subastas.js
@@ -5,7 +5,6 @@ subastasRoutes.get('/', subastasController.getAllSubastas);
 subastasRoutes.get('/cliente/:id', subastasController.getAllSubastasByIdCliente);
 subastasRoutes.get('/catalogo/:id', subastasController.getCatalogoBySubastaId);
 subastasRoutes.get('/catalogo/:id/item-catalogo', subastasController.getItemSubastandoseBySubastaId);
-subastasRoutes.get('/:idSubasta/cliente/:idCliente/catalogo', subastasController.getCatalogoBySubastaIdAndIdCliente);
 subastasRoutes.post('/catalogo/change-item-estado', subastasController.changeEstadoItemSubastandose);
 
 module.exports = subastasRoutes;
